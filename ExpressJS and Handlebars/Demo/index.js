1f3a231eb401361e51fcdb02ad173fd9ed335c33
Krasimir Ivanov <k.ivanov29@yahoo.com>
2023-01-13 19:21:58 +0200

    Testing handlebar helpers

--- ExpressJS and Handlebars/Demo/index.js
+++ ExpressJS and Handlebars/Demo/index.js
@@ -10,5 +10,32 @@ const app = express();
 app.engine(".hbs", handlebars.engine);
 app.set("view engine", ".hbs");
 
+app.get("/", (req, res) => {
+  res.render("home", {
+    username: "Gosho",
+    title: "Handlebars Demo",
+    message: "Its working",
+    product: {
+      name: "Product 1",
+      price: 21.99,
+      color: "brown",
+    },
+    contacts: [
+      {
+        name: "John",
+        email: "john@abv.bg",
+      },
+      {
+        name: "Mary",
+        email: "mary@abv.bg",
+      },
+      {
+        name: "Bob",
+        email: "bob@abv.bg",
+      },
+    ],
+  });
+});
+
 app.listen(5000);
 console.log(`Server is running on port 5000`);
