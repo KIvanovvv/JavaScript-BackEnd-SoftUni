c16b02814a271d2b8f331c0ba6a7bd6ca036ab20
Krasimir Ivanov <k.ivanov29@yahoo.com>
2023-01-31 22:29:26 +0200

    Adding updateNav middleware

--- ExamPrep/CryptoTrade/config/express.js
+++ ExamPrep/CryptoTrade/config/express.js
@@ -2,6 +2,7 @@ const express = require("express");
 const hbs = require("express-handlebars").create({ extname: ".hbs" });
 const cookieParser = require("cookie-parser");
 const tokenValidation = require("../middleware/tokenValidation.js");
+const updateNav = require("../middleware/updateNav.js");
 const app = express();
 
 app.engine(".hbs", hbs.engine);
@@ -10,5 +11,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/static", express.static("static"));
 app.use(tokenValidation());
-
+app.use(updateNav());
 module.exports = app;
